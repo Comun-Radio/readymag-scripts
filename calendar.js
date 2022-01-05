ee44ee733e06c29ba8b9633b350a9e25953b44a2
Roy <roy.jara224@gmail.com>
2022-01-05 10:23:19 -0500

    cal edits - working logic+html

--- calendar.js
+++ calendar.js
@@ -2,22 +2,41 @@
   /* <script> */
 }
 
-fetch("https://ef64-137-184-63-208.ngrok.io/upcoming")
+// artistas text box dataid - 617f4d760b735e0020d90504
+
+// make periodic fetches - every 5 min ?
+//  6:05, 6:10, 6:15 if it starts anytime between 6:00 and 6:04:59
+
+fetch("https://2586-137-184-63-208.ngrok.io/upcoming")
   .then((res) => {
-    console.log("hello from Digital Ocean", res);
     return res.json();
   })
   .then((arr) => {
-    const titles = arr.map((item) => item.song.title);
-    console.log(titles);
     var tzoffset = new Date().getTimezoneOffset() * 60000; //offset in milliseconds
-    const times = arr.map((item) =>
-      new Date(item.cued_at * 1000 - tzoffset)
+    const queue = arr.map((item) => ({
+      title: item.song.title,
+      time: new Date(item.cued_at * 1000 - tzoffset)
         .toISOString()
         .slice(0, 19)
-        .replace("T", " ")
+        .replace("T", " "),
+    }));
+    queue.unshift({
+      title: "Artistas",
+      time: "a Horas",
+    });
+
+    let calendarHTML = "";
+    for (let i = 0; i < 5; i++) {
+      calendarHTML += `
+        <div class="flex">
+          <h2 class="flex-child title">${queue[i].title.toUpperCase()}<\h2>
+          <h2 class="flex-child time">${queue[i].time.split(" ")[1]}<\h2>
+  		  </div>`;
+    }
+    const container = document.querySelector(
+      'div[data-id="617f4d760b735e0020d90504"] p'
     );
-    console.log(times);
+    container.innerHTML = calendarHTML;
   })
   .catch((err) => console.log(err));
 
